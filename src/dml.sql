e41fee48efd2d85d40eb801201020389c4ce094d
João G. S. Oliveira <gjoao1821@gmail.com>
2026-09-23 08:29:37 -0300

    Create DML
    criação do arquivo base do DML e primeiros inserts

---
+++ src/dml.sql
@@ -0,0 +1,13 @@
+insert into p2p_usuario (NOME, USERNAME, EMAIL, SENHA)
+values('João', 'sone_jojo', 'sonejojo@gmail.com', 'aaaa1111' );
+insert into p2p_usuario (NOME, USERNAME, EMAIL, SENHA)
+values('Maria', 'marinr', 'marin@gmail.com', 'aaaa1111' );
+
+select * from p2p_usuario;
+
+insert into p2p_categoria (COD_CATEGORIA, NOME, TIPO)
+values('LSWD', 'Espada Longa', 1);
+insert into p2p_categoria (COD_CATEGORIA, NOME, TIPO)
+values('SSWD', 'Espada Curta', 1);
+
+select * from p2p_categoria;
